f1a8ccc50e485bfb20b503f5fa3e4478a4d2d959
tarekmonjur <tarekmonjur@gmail.com>
2019-02-06 12:00:28 +0600

    add middleare to controller

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -4,6 +4,17 @@ namespace App\Http\Controllers;
 
 class AttendanceController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
 	public function index(){
 
 		return view('admin.attendance_reports');
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 class StudentController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
 
 	public function index(){
