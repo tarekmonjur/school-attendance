25cff68a54ceb3d3bd050b30a22e253e44519c9c
tarekmonjur <tarekmonjur@gmail.com>
2019-02-19 11:41:38 +0600

    add settings migration

---
+++ database/migrations/2019_02_19_113852_create_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('in_sms')->default(0);
+            $table->tinyInteger('out_sms')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
