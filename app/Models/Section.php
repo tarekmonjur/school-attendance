d5afce5bbea1bbc819c28502146681780c3aac97
tarekmonjur <tarekmonjur@gmail.com>
2019-04-10 11:21:39 +0600

    add section migration and model

---
+++ app/Models/Section.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monjur
+ * Date: 2/4/19
+ * Time: 10:58 AM
+ */
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    protected $table = "section";
+
+}
